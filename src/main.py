8a776273da058edd2a778a7ac12e873d589ee745
Newton Gonzaga Costa <ncosta.rj@gmail.com>
2025-08-29 23:22:58 -0300

    adicionando comentarios para melhor entender o codigo

--- src/main.py
+++ src/main.py
@@ -1,5 +1,10 @@
 from models.LeitorXLS import LeitorXLS
 
+# iniciando o leitor de arquivos
 leitor = LeitorXLS('storage/xls/lista_compras.xlsx')
+
+# tratando os dados
 leitor.tratarDados()
+
+# salvando o arquivo tratado
 leitor.salvarDados('storage/csv/lista_compras.csv')
